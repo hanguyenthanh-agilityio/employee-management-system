dce9604b43b41e50cbf0e451d35c4069b1c61e67
ha nguyen thanh <ha.nguyenthanh@asnet.com.vn>
2025-04-22 16:33:24 +0700

    Update style for table

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -6,8 +6,8 @@ interface TableProps {
 export default function Table({ headers, rows }: TableProps) {
   return (
     <div className="overflow-x-auto">
-      <table className="w-full text-sm text-left text-[#545559] min-w-[500px] border-separate border-spacing-y-3">
-        <thead className="bg-[#E3EDF9]">
+      <table className="w-full text-xl text-left text-[#545559] min-w-[500px] border-separate border-spacing-y-3">
+        <thead className="bg-[#E3EDF9] h-[60px]">
           <tr>
             {headers.map((head, index) => (
               <th key={index} className="px-4 py-3 font-medium">
@@ -20,7 +20,7 @@ export default function Table({ headers, rows }: TableProps) {
           {rows.map((row, idx) => (
             <tr
               key={idx}
-              className="bg-[#E3EDF9] border-b border-white rounded-[10px]"
+              className="bg-[#E3EDF9] border-b border-white rounded-[10px] h-[60px]"
             >
               {row.map((cell, index) => (
                 <td key={index} className="px-4 py-3 whitespace-nowrap">
