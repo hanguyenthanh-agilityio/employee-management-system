e22d3b942584bcc3144ee7a85ef8d0cd89c1bbad
ha nguyen thanh <ha.nguyenthanh@asnet.com.vn>
2025-04-25 18:11:40 +0700

    Handle login

--- src/app/category/[id]/page.tsx
+++ src/app/category/[id]/page.tsx
@@ -1,39 +1,40 @@
 import { Metadata } from 'next';
-import { Suspense } from 'react';
-import Link from 'next/link';
+// import { Suspense } from 'react';
+// import Link from 'next/link';
 
-// Constants
-import { ROUTER } from '@/constants/router';
+// // Constants
+// import { ROUTER } from '@/constants/router';
 
-// Components
-import { ChevronRightIcon } from '@/icons';
-import { CategoryDetail, CategoryDetailSkeleton } from '@/ui/category';
+// // Components
+// import { ChevronRightIcon } from '@/icons';
+// import { CategoryDetail, CategoryDetailSkeleton } from '@/ui/category';
 
 export const metadata: Metadata = {
   title: 'Category Detail',
 };
 
-const CategoryDetailPage = async ({ params }: { params: { id: string } }) => {
-  const id = params.id;
+// const CategoryDetailPage = async ({ params }: { params: { id: string } }) => {
+const CategoryDetailPage = async () => {
+  // const id = params.id;
 
   return (
     <main className="">
       <div className="px-6">
-        <Link
+        {/* <Link
           href={ROUTER.CATEGORY}
           title="Category List"
           className="flex items-center text-blue-500 hover:text-blue-800 mb-4"
         >
-          <ChevronRightIcon customClass="w-6 h-6 rotate-180" />
-          Category List
-        </Link>
+          <ChevronRightIcon customClass="w-6 h-6 rotate-180" /> */}
+        Category List
+        {/* </Link>
 
         <Suspense
           key={`${ROUTER.CATEGORY}/${id}`}
           fallback={<CategoryDetailSkeleton />}
         >
           <CategoryDetail id={id} />
-        </Suspense>
+        </Suspense> */}
       </div>
     </main>
   );
